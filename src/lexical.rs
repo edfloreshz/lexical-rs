120cf6c6b9cbad9a358a29ea23a6bbb6b42734f5
Eduardo Flores <edfloreshz@gmail.com>
2023-05-23 02:28:56 -0700

    Added state enum

--- src/lexical.rs
+++ src/lexical.rs
@@ -1,12 +1,25 @@
-use std::path::PathBuf;
-use std::io::{BufRead, BufReader};
-use std::fs::File;
 use std::env;
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+use std::path::PathBuf;
 
 use crate::token::{Token, Tokens};
 
-#[derive(Clone)]
+enum LexemeState {
+    None,
+    GreaterOrLowerThan,
+    EqualsEquals,
+    NotEqualTo,
+    CommentStart,
+    SingleLineComment,
+    MultiLineComment,
+    MultiLineClose,
+    Number,
+    Alphabetic,
+}
+
 pub struct Lexical {
+    state: LexemeState,
     pub variables: u32,
     pub constants: u32,
     pub assignments: u32,
@@ -19,7 +32,7 @@ pub struct Lexical {
     pub input: PathBuf,
     pub buffer: String,
     pub forward: usize,
-    pub initial: u32
+    pub initial: u32,
 }
 
 impl Lexical {
@@ -27,6 +40,7 @@ impl Lexical {
         let mut input = env::current_dir().unwrap();
         input.push("src/input.txt");
         Lexical {
+            state: LexemeState::None,
             variables: 0,
             constants: 0,
             assignments: 0,
@@ -39,7 +53,7 @@ impl Lexical {
             input,
             buffer: String::new(),
             forward: 0,
-            initial: 0 
+            initial: 0,
         }
     }
     pub fn next_char(&mut self) -> char {
@@ -59,64 +73,62 @@ impl Lexical {
         self.forward -= 1
     }
     pub fn set_lexeme(&mut self) -> Token {
-        let mut state: u32 = 0;
+        self.state = LexemeState::None;
         let mut character: char;
         let mut lexeme = String::new();
         loop {
             character = self.next_char();
-            match state {
-                0 => {
-                    match character {
-                        '\r' => (),
-                        '\t' => (),
-                        ' ' => (),
-                        '\0' => self.retract(),
-                        '\n' => {self.line += 1},
-                        ';' => return Token::new(Tokens::EndOfStatement, character.to_string()),
-                        '(' => return Token::new(Tokens::OpenParenthesis, character.to_string()),
-                        ')' => return Token::new(Tokens::ClosedParenthesis, character.to_string()),
-                        '{' => return Token::new(Tokens::OpenBracket, character.to_string()),
-                        '}' => {
-                            self.main_line_ends = self.line;
-                            return Token::new_single(Tokens::ClosedBracket)
-                        },
-                        '*' => return Token::new(Tokens::ArithmeticOperator, character.to_string()),
-                        '+' => return Token::new(Tokens::ArithmeticOperator, character.to_string()),
-                        '-' => return Token::new(Tokens::ArithmeticOperator, character.to_string()),
-                        '/' => {state = 6},
-                        '>' | '<' => {
-                            state = 1;
-                            lexeme += character.to_string().as_str();
-                        },
-                        '=' => {state = 2},
-                        '!' => {state = 3},
-                        '~' => {
-                            self.main_line_ends = self.main_line_start + self.main_line_ends;
-                            return Token::new_single(Tokens::Eof)
-                        },
-                        _ => {
-                            if character.is_alphabetic() || character == '_' {
-                                state = 5;
-                                lexeme += character.to_string().as_str()
-                            } else if character.is_digit(10) {
-                                state = 4;
-                                lexeme += character.to_string().as_str()
-                            } else {
-                                return Token::new(Tokens::Error, String::from("Simbolo Indefinido"));
-                            }
+            match self.state {
+                LexemeState::None => match character {
+                    '\r' => (),
+                    '\t' => (),
+                    ' ' => (),
+                    '\0' => self.retract(),
+                    '\n' => self.line += 1,
+                    ';' => return Token::new(Tokens::EndOfStatement, character.to_string()),
+                    '(' => return Token::new(Tokens::OpenParenthesis, character.to_string()),
+                    ')' => return Token::new(Tokens::ClosedParenthesis, character.to_string()),
+                    '{' => return Token::new(Tokens::OpenBracket, character.to_string()),
+                    '}' => {
+                        self.main_line_ends = self.line;
+                        return Token::new_single(Tokens::ClosedBracket);
+                    }
+                    '*' => return Token::new(Tokens::ArithmeticOperator, character.to_string()),
+                    '+' => return Token::new(Tokens::ArithmeticOperator, character.to_string()),
+                    '-' => return Token::new(Tokens::ArithmeticOperator, character.to_string()),
+                    '/' => self.state = LexemeState::CommentStart,
+                    '>' | '<' => {
+                        self.state = LexemeState::GreaterOrLowerThan;
+                        lexeme += character.to_string().as_str();
+                    }
+                    '=' => self.state = LexemeState::EqualsEquals,
+                    '!' => self.state = LexemeState::NotEqualTo,
+                    '~' => {
+                        self.main_line_ends = self.main_line_start + self.main_line_ends;
+                        return Token::new_single(Tokens::Eof);
+                    }
+                    _ => {
+                        if character.is_alphabetic() || character == '_' {
+                            self.state = LexemeState::Alphabetic;
+                            lexeme += character.to_string().as_str()
+                        } else if character.is_digit(10) {
+                            self.state = LexemeState::Number;
+                            lexeme += character.to_string().as_str()
+                        } else {
+                            return Token::new(Tokens::Error, String::from("Simbolo Indefinido"));
                         }
                     }
-                }
-                1 => {
+                },
+                LexemeState::GreaterOrLowerThan => {
                     if character == '=' {
                         lexeme += character.to_string().as_str();
                     } else {
                         self.retract();
                         self.conditionals += 1;
                     }
-                    return Token::new(Tokens::RelationalOperator, lexeme)
+                    return Token::new(Tokens::RelationalOperator, lexeme);
                 }
-                2 => {
+                LexemeState::EqualsEquals => {
                     return if character == '=' {
                         self.conditionals += 1;
                         Token::new(Tokens::RelationalOperator, String::from("=="))
@@ -126,28 +138,32 @@ impl Lexical {
                         Token::new(Tokens::Assignment, String::from("="))
                     }
                 }
-                3 => {
+                LexemeState::NotEqualTo => {
                     return if character == '=' {
                         self.conditionals += 1;
                         Token::new(Tokens::RelationalOperator, String::from("!="))
                     } else {
                         self.retract();
-                        Token::new(Tokens::Error, String::from("Se esperaba '=' despues de '!'"))
+                        Token::new(
+                            Tokens::Error,
+                            String::from("Se esperaba '=' despues de '!'"),
+                        )
                     }
                 }
-                4 => {
+                LexemeState::Number => {
                     if !character.is_digit(10) {
                         self.retract();
                         return Token::new(Tokens::NaturalNumber, lexeme);
                     }
                     lexeme += character.to_string().as_str();
                 }
-                5 => {
+                LexemeState::Alphabetic => {
                     if character.is_alphabetic() || character.is_digit(10) || character.eq(&'_') {
                         lexeme += character.to_string().as_str();
                     } else {
                         if KEYWORDS.contains(&lexeme.as_str()) {
-                            if lexeme.to_lowercase().eq("for") || lexeme.to_lowercase().eq("while") {
+                            if lexeme.to_lowercase().eq("for") || lexeme.to_lowercase().eq("while")
+                            {
                                 self.loops += 1;
                             } else if lexeme.to_lowercase().eq("const") {
                                 self.constants += 1;
@@ -164,54 +180,53 @@ impl Lexical {
                         } else {
                             self.retract();
                             Token::new(Tokens::Identifier, lexeme)
-                        }
+                        };
                     }
                 }
-                6 => {
+                LexemeState::CommentStart => {
                     if character.eq(&'/') {
-                        state = 7
+                        self.state = LexemeState::SingleLineComment;
                     } else if character.eq(&'*') {
-                        state = 8
+                        self.state = LexemeState::MultiLineComment
                     } else {
                         self.retract();
-                        return Token::new(Tokens::ArithmeticOperator, String::from("/"))
+                        return Token::new(Tokens::ArithmeticOperator, String::from("/"));
                     }
                 }
-                7 => {
+                LexemeState::SingleLineComment => {
                     if character.eq(&'\n') {
-                        state = 0;
+                        self.state = LexemeState::None;
                         self.line += 1;
                         self.comments += 1;
-                        return Token::new(Tokens::LineComment, lexeme)
+                        return Token::new(Tokens::LineComment, lexeme);
                     } else {
                         lexeme += character.to_string().as_str()
                     }
                 }
-                8 => {
+                LexemeState::MultiLineComment => {
                     if !character.eq(&'*') && character.is_alphabetic() || character.eq(&'_') {
                         lexeme += character.to_string().as_str()
                     }
                     if character.eq(&'\n') {
                         self.line += 1;
                     } else if character.eq(&'*') {
-                        state = 9;
+                        self.state = LexemeState::MultiLineClose;
                     }
                 }
-                9 => {
+                LexemeState::MultiLineClose => {
                     if !character.eq(&'/') {
                         if character.is_alphabetic() || character.eq(&'_') {
                             lexeme += character.to_string().as_str()
                         } else if character.eq(&'\n') {
                             self.line += 1;
                         }
-                        state = 8
+                        self.state = LexemeState::MultiLineComment;
                     } else {
-                        state = 0;
+                        self.state = LexemeState::None;
                         self.comments += 1;
-                        return Token::new(Tokens::MultilineComment, lexeme)
+                        return Token::new(Tokens::MultilineComment, lexeme);
                     }
                 }
-                _ => {}
             }
         }
     }
@@ -239,15 +254,8 @@ const KEYWORDS: &'static [&str; 22] = &[
     "try",
     "catch",
     "return",
-    "public"
+    "public",
 ];
 
-const DATA_TYPES: &'static [&str; 7]= &[
-    "char",
-    "int",
-    "long",
-    "double",
-    "string",
-    "short",
-    "bool",
-];
+const DATA_TYPES: &'static [&str; 7] =
+    &["char", "int", "long", "double", "string", "short", "bool"];
